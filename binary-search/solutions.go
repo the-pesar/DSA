60c1683344249b03e24b52ea3a4a097499aa552f
yasha <pesar369@gmail.com>
2024-11-21 20:38:46 +0330

    add binary-search solutions

---
+++ binary-search/solutions.go
@@ -0,0 +1,20 @@
+package binarysearch
+
+// recursive + two pointer(?)
+func BinarySearch(nums []int, target int, left int, right int) int {
+	if left > right {
+		return -1
+	}
+
+	mid := left + (right-left)/2
+
+	if nums[mid] == target {
+		return mid
+	}
+
+	if nums[mid] < target {
+		return BinarySearch(nums, target, mid+1, right)
+	} else {
+		return BinarySearch(nums, target, left, mid-1)
+	}
+}
